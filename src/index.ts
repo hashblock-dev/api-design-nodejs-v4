e079e5535a533b24768178a63aba1c8ef65f85c1
Cedric <cedric@hashblock.dev>
2025-04-14 22:55:44 +0200

    chore: install prisma, typescript and ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const app = require("./server");
+import app from "./server";
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
 
-module.exports = app;
+export default app;
