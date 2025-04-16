943e31deb5b587e08265f6562d1fd57da31fb404
Cedric <cedric@hashblock.dev>
2025-04-17 00:08:15 +0200

    feat: add some middleware

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,13 @@ import router from "./router";
 
 const app = express();
 
+// Middleware
+// Log requests to the console
 app.use(morgan("dev"));
+// Parse incoming requests with JSON payloads
+app.use(express.json());
+// Parse incoming requests with URL-encoded payloads
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
